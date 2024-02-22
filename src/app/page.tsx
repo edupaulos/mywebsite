a502ad36b079b488f7f10a0f85aab77e27c3a75b
Eduardo Paulos <eduardo.paulos@farfetch.com>
2024-02-22 00:56:25 +0000

    feat: add font family and icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,22 @@ import Box from '@/components/Box';
 export default function Home() {
     return (
         <main>
-            <Box className="flex justify-center items-center flex-col animate-initialAnimation bg-primary-color">
-                <h1 className="font-bold text-white text-center">
+            <Box className="relative flex flex-col justify-center items-center animate-initialAnimation bg-primary-color">
+                <h1 className="font-black text-white text-center break-inside-avoid">
                     Hey, there!
-                    <p />
-                    I’m Eduardo.
+                    <br />
+                    I’m{' '}
+                    <span className="hover:underline decoration-wavy underline-offset-8">
+                        Eduardo.
+                    </span>
                 </h1>
-                <p className="text-3xl">I'm a Web developer.</p>
+                <p className="text-5xl">
+                    Welcome to my corner of the internet!
+                </p>
+                <img
+                    className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-96"
+                    src="/icons/Vector.svg"
+                />
             </Box>
         </main>
     );
