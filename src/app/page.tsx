155f9d2277b240fce374d398be103a665a6c554d
Eduardo Paulos <eduardo.paulos@farfetch.com>
2024-02-23 17:02:03 +0000

    feat: add navBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,31 @@
 'use client';
 
 import Box from '@/components/Box';
+import { NavBar } from '@/components/NavBar';
 
 export default function Home() {
     return (
-        <main>
-            <Box className="relative flex flex-col justify-center items-center animate-initialAnimation bg-primary-color">
-                <h1 className="font-black text-white text-center break-inside-avoid">
-                    Hey, there!
-                    <br />
-                    I’m{' '}
-                    <span className="hover:underline decoration-wavy underline-offset-8">
-                        Eduardo.
-                    </span>
-                </h1>
-                <p className="text-5xl">
-                    Welcome to my corner of the internet!
-                </p>
-                <img
-                    className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-96"
-                    src="/icons/Vector.svg"
-                />
-            </Box>
-        </main>
+        <>
+            <NavBar />
+            <main>
+                <Box className="relative flex animate-initialAnimation flex-col items-center justify-center bg-primary-color">
+                    <h1 className="break-inside-avoid text-center font-black text-white">
+                        Hey, there!
+                        <br />
+                        I’m{' '}
+                        <span className="decoration-wavy underline-offset-8 hover:underline">
+                            Eduardo.
+                        </span>
+                    </h1>
+                    <p className="text-4xl">
+                        Welcome to my corner of the internet!
+                    </p>
+                    <img
+                        className="absolute left-1/2 top-1/2 mt-96 -translate-x-1/2 -translate-y-1/2 "
+                        src="/icons/Vector.svg"
+                    />
+                </Box>
+            </main>
+        </>
     );
 }
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import Link from 'next/link';
+import { useState } from 'react';
 
-// import { Container } from './styles';
+export const NavBar: React.FC = () => {
+    const [hoveredLink, setHoveredLink] = useState<HTMLElement | null>(null);
+    const [isHovered, setIsHovered] = useState(false);
 
-const NavBar: React.FC = () => {
-  return <div />;
-}
+    const handleLinkEnter = (id: string) => {
+        setHoveredLink(document.getElementById(`${id}-link`) as HTMLElement);
+    };
 
-export default NavBar;
+    return (
+        <nav
+            className="group fixed left-1/2 top-24 z-10 flex -translate-x-1/2 -translate-y-1/2 gap-12 rounded-full bg-white/50 px-10 py-5 text-lg backdrop-blur-md"
+            onMouseLeave={() => {
+                setIsHovered(false);
+            }}
+            onMouseEnter={() => setIsHovered(true)}
+        >
+            {hoveredLink && (
+                <div
+                    className="absolute rounded-full bg-white p-2 opacity-80 transition-all"
+                    style={{
+                        width: hoveredLink.offsetWidth,
+                        bottom: '0',
+                        left: hoveredLink.offsetLeft,
+                        transform: `translateX(0) scale(${isHovered ? 1 : 0})`,
+                    }}
+                ></div>
+            )}
+
+            {['home', 'about', 'blog', 'contacts'].map((id) => (
+                <Link key={id} href={`#${id}`}>
+                    <div
+                        onMouseEnter={() => handleLinkEnter(id)}
+                        id={`${id}-link`}
+                        className="text-white transition-all"
+                        style={{
+                            opacity:
+                                isHovered && hoveredLink?.id === `${id}-link`
+                                    ? 0.7
+                                    : 1,
+                            // Add any other styles or transitions for links
+                        }}
+                    >
+                        {id.charAt(0).toUpperCase() + id.slice(1)}
+                    </div>
+                </Link>
+            ))}
+        </nav>
+    );
+};
