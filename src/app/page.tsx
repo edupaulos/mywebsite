54cffe1a405263d27bcc8363e49c2908f09abb41
Eduardo Paulos <edupaulos1@gmail.com>
2024-04-18 15:30:02 +0100

    feat: add noise background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 'use client';
 
+import { About } from '@/components/About';
 import { Boobles } from '@/components/ui/Boobles';
 import Image from 'next/image';
 
 export default function Home() {
     return (
-        <main className="mx-auto">
+        <main className=" mx-auto">
             <Boobles />
 
-            <div className="relative flex animate-initialAnimation flex-col items-center justify-center bg-white/20 bg-blend-overlay backdrop-blur-3xl dark:bg-black/30">
+            {/* <img
+                src="/noise.svg"
+                className="absolute size-40 animate-initialAnimation mix-blend-overlay"
+            /> */}
+
+            <div className="absolute left-1/2 top-1/2 z-10 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center">
                 <h1 className="inline break-inside-avoid text-center font-black text-white">
                     Hey, there!
                     <br />
@@ -17,11 +23,12 @@ export default function Home() {
                 <p className="text-2xl">
                     Welcome to my corner of the internet!
                 </p>
-                <img
-                    className="absolute left-1/2 top-1/2 mt-64 -translate-x-1/2 -translate-y-1/2 "
-                    src="/icons/Vector.svg"
-                />
+                <img className="p-10" src="/icons/Vector.svg" />
             </div>
+
+            <div className="animate-initialAnimation bg-noise-pattern bg-fixed mix-blend-soft-light backdrop-blur-3xl dark:bg-white/30" />
+
+            <About></About>
 
             <footer></footer>
         </main>
